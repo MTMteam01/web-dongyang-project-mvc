4df7c45a760eefbd74b466d498677e84cc8341a6
Gwonhoemin <hoemin06@naver.com>
2023-12-04 01:09:45 +0900

    feat : "회원가입"

---
+++ WebProjectMovie/src/main/java/MovieDB/MemberDAO.java
@@ -0,0 +1,128 @@
+package MovieDB;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+import common.JDBCUtil;
+
+
+
+public class MemberDAO {
+
+    public boolean loginCheck(String id, String password) {
+    	Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        boolean loginCon = false;
+        
+        try {
+			conn = JDBCUtil.getConnection();
+            String strQuery = "select id, password from users where id = ? and password = ?";
+
+            pstmt = conn.prepareStatement(strQuery);
+            pstmt.setString(1, id);
+            pstmt.setString(2, password);
+            rs = pstmt.executeQuery();
+            loginCon = rs.next();
+        } catch (Exception ex) {
+            System.out.println("Exception" + ex);
+        } finally {
+        	JDBCUtil.close(rs, pstmt, conn);
+        }
+        
+        return loginCon;
+    }	
+	
+	
+    public boolean userInsert(MemberDTO mDTO) {
+    	Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        boolean flag = false;
+        
+        try {
+        	conn = JDBCUtil.getConnection();
+            String strQuery = "insert into users values(?,?,?,?,?)";
+            pstmt = conn.prepareStatement(strQuery);
+            pstmt.setString(1, mDTO.getId());
+            pstmt.setString(2, mDTO.getPassword());
+            pstmt.setString(3, mDTO.getName());
+            pstmt.setString(4, mDTO.getPhone());
+            pstmt.setInt(5, mDTO.getAuth());
+
+            int count = pstmt.executeUpdate();
+
+            if (count == 1) {
+                flag = true;
+            }
+
+        } catch (Exception ex) {
+            System.out.println("Exception" + ex);
+        } finally {
+        	JDBCUtil.close(rs, pstmt, conn);
+        }
+        
+        return flag;
+    }
+    
+    public boolean userDelete(String id) {
+    	Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        boolean flag = false;
+        
+        try {
+        	conn = JDBCUtil.getConnection();
+            String strQuery = "delete from users where id = ?";
+            pstmt = conn.prepareStatement(strQuery);
+            pstmt.setString(1, id);
+
+            int count = pstmt.executeUpdate();
+
+            if (count == 1) {
+                flag = true;
+            }
+
+        } catch (Exception ex) {
+            System.out.println("Exception" + ex);
+        } finally {
+        	JDBCUtil.close(rs, pstmt, conn);
+        }
+        
+        return flag;
+    }
+    
+    public boolean userUpdate(String id, String password, String name, String phone) {
+    	Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        boolean flag = false;
+        
+        try {
+        	conn = JDBCUtil.getConnection();
+            String strQuery = "update users set password = ?, name = ?, phone = ? where id = ?";
+            pstmt = conn.prepareStatement(strQuery);
+            pstmt.setString(1, password);
+            pstmt.setString(2, name);
+            pstmt.setString(3, phone);
+            pstmt.setString(4, id);
+
+            int count = pstmt.executeUpdate();
+
+            if (count == 1) {
+                flag = true;
+            }
+
+        } catch (Exception ex) {
+            System.out.println("Exception" + ex);
+        } finally {
+        	JDBCUtil.close(rs, pstmt, conn);
+        }
+        
+        return flag;
+    	
+    }
+}
---
+++ WebProjectMovie/src/main/java/MovieDB/MemberDTO.java
@@ -0,0 +1,45 @@
+package MovieDB;
+
+public class MemberDTO {
+
+    private String id;
+    private String password;
+    private String name;
+    private String phone;
+    private int authority;
+    
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String passwordd) {
+		this.password = passwordd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public int getAuth() {
+		return authority;
+	}
+	public void setAuth(int Auth) {
+		this.authority = Auth;
+	}
+
+
+
+}
+
