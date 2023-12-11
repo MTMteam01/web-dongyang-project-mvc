b178aff0a70d87ce444622b9e8fa24ea62d97342
MJ <kminj020927@naver.com>
2023-12-11 20:13:24 +0900

    indexForm

--- WebProjectMovie/src/main/java/test/ReservationServlet.java
+++ WebProjectMovie/src/main/java/test/ReservationServlet.java
@@ -22,7 +22,7 @@ public class ReservationServlet extends HttpServlet {
         // 사용자 ID가 없으면 로그인 페이지로 리다이렉트
         if (userId == null) {
 
-            response.sendRedirect("loginForm.jsp");
+            response.sendRedirect("../login/loginForm.jsp");
 
             return;
         }
--- WebProjectMovie/src/main/webapp/home/indexScript.js
+++ WebProjectMovie/src/main/webapp/home/indexScript.js
@@ -23,3 +23,36 @@ function showSlides() {
 
 // Initial call to start the slideshow
 showSlides();
+// 왼쪽 버튼 이미지 클릭 시 동작 how
+window.addEventListener("load", ()=>{
+    var leftBtn = document.querySelector(".leftbtn");
+    var arrImg = ['/mainImg1.jpg', '/mainImg2.jpg', '/mainImg3.jpg'];
+    var cntleft = 1;
+    leftBtn.onclick = function() {
+        var mainImg = document.querySelector("#main_image");
+        if(cntleft >= arrImg.length){
+            cntleft = 0;
+        }
+        cntleft++;
+        mainImg.style.backgroundImage = "url(" + arrImg[cntleft-1] + ")";
+        /* element css 속성 변경하는 법 object.style.attribute
+         원래 css 파일에선 background-image 라고 작성하지만,
+         javascript에선 하이픈(-) 권장하지 않기 때문에 대문자로 대체ㅣ
+        */
+    }
+    
+});
+// 오른 쪽 버튼 이미지 클릭 시 동작 how
+window.addEventListener("load", ()=>{
+    var rightBtn = document.querySelector(".rightbtn");
+    var arrImg = ['/mainImg1.jpg', '/mainImg2.jpg', '/mainImg3.jpg'];
+    var cntright = arrImg.length;
+    rightBtn.onclick = function() {
+        var mainImg = document.querySelector("#main_image");
+        cntright--;
+        if(cntright < 0){
+            cntright = arrImg.length-1;
+        }
+        mainImg.style.backgroundImage = "url(" + arrImg[cntright] + ")";
+    }
+});
